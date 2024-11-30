5109703bed79bbaef0b669183a18a8401e100b6e
Kyltetran <138639043+Kyltetran@users.noreply.github.com>
2024-11-30 11:38:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ForumPage from './components/ForumPage';
 import './styles/style.css'; // Import your CSS file
@@ -12,15 +12,22 @@ function App() {
         {/* Navigation Bar */}
         <nav className="navbar">
           <div className="logo">Group 2</div>
-          <div className="nav-links">
-            <Link to="/" className="nav-link">Home</Link>
-            <Link to="/forum" className="nav-link">Forum</Link>
+              <div className="nav-links">
+                  <a href="#home" className="active">Home</a>
+                  <a href="/forum" id="forum-link">Forum</a>
+                  <a href="#local-matching">Local Matching</a>
+                  <a href="#plan-food-tour">Plan Food Tour</a>
+                  <a href="#pricing">Pricing</a>
+              </div>
+              <div className="auth-links">
+                  <a href="#sign-up">Sign Up</a>
+                  <a href="#log-in">Log In</a>
           </div>
         </nav>
 
         {/* Routes for Home and Forum */}
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/forum" element={<ForumPage />} />
         </Routes>
       </div>
